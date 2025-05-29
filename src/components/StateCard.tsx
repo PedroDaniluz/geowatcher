69519b300ac54b5546a6b15577192e24b49eaaae
Pedro Daniluz <pedrodaniluz04@gmail.com>
2025-05-28 23:54:41 -0300

    feat: add StateCard component and integrate it into Home screen; update package dependencies

---
+++ src/components/StateCard.tsx
@@ -0,0 +1,58 @@
+import { Text, View } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+import styled from "styled-components";
+import theme from "../styles/theme";
+
+interface CardProps {
+    isSafe: boolean;
+}
+
+const StateCard: React.FC<CardProps> = ({ isSafe }) => {
+    return (
+        <Card>
+            <CardText>
+                <CardHeader>Estado atual</CardHeader>
+                <CardState isSafe={isSafe}>{isSafe ? 'Seguro' : 'Risco'}</CardState>
+            </CardText>
+            {isSafe ?
+                <Ionicons name="shield-checkmark" size={80} color={theme.colors.secundary} />
+                :
+                <Ionicons name="warning" size={80} color="red" />}
+        </Card>
+    )
+}
+
+const Card = styled(View)`
+    width: 100%;
+    padding: 16px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: ${theme.colors.background};
+    border: 0.5px solid ${theme.colors.primary};
+    border-radius: 8px;
+    shadow-color: #000;
+    shadow-opacity: 0.1;
+    shadow-radius: 4px;
+    shadow-offset: 0px 2px;
+    elevation: 4;
+`;
+
+const CardText = styled(View)`
+    flex: 1;
+    gap: 8px;
+`;
+
+const CardHeader = styled(Text)`
+    color: ${theme.colors.primary};
+    font-family: ${theme.fonts.bold};
+    font-size: 20px;
+`;
+
+const CardState = styled(Text) <{ isSafe?: boolean }>`
+    color: ${({ isSafe }) => isSafe ? theme.colors.secundary : 'red'};
+    font-family: ${theme.fonts.extraBold};
+    font-size: 44px;
+`;
+
+export default StateCard;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,13 +1,28 @@
 import { View, Text } from "react-native";
+import theme from "../styles/theme";
+import styled from "styled-components";
+import StateCard from "../components/StateCard";
 
 const Home = () => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-                Home Screen
-            </Text>
-        </View>
+        <Container>
+            <Header>Painel de Riscos</Header>
+            <StateCard isSafe = {true} />
+        </Container>
     );
 }
+
+const Container = styled(View)`
+    flex: 1;
+    padding: 72px 32px;
+    background-color: ${theme.colors.background};
+`;
+
+const Header = styled(Text)`
+    color: ${theme.colors.primary};
+    font-family: ${theme.fonts.bold};
+    font-size: 28px;
+    margin-bottom: 24px;
+`;
 
 export default Home;
